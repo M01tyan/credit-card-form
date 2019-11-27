998829e08817fb4bb06e39eda4101d071c2022ee
前田幹太 <maedakanta@M01tyan.local>
2019-11-27 22:05:02 +0900

    Updates

---
+++ precache-manifest.f1693332846cde0b632ae82b2ad7d994.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8654e9dfc768a694f98fed505deee21",
+    "url": "/credit-card-form/index.html"
+  },
+  {
+    "revision": "49c56f450c2f16fb7b36",
+    "url": "/credit-card-form/static/css/main.c6f5d0d0.chunk.css"
+  },
+  {
+    "revision": "ff6c92fdc8fc2318803c",
+    "url": "/credit-card-form/static/js/2.a854df48.chunk.js"
+  },
+  {
+    "revision": "49c56f450c2f16fb7b36",
+    "url": "/credit-card-form/static/js/main.96c56814.chunk.js"
+  },
+  {
+    "revision": "5244785206cf500b97bb",
+    "url": "/credit-card-form/static/js/runtime-main.504e548f.js"
+  },
+  {
+    "revision": "2c37b1d33b45b2158112bb8172af579b",
+    "url": "/credit-card-form/static/media/chip.2c37b1d3.png"
+  },
+  {
+    "revision": "62706277b8600fc43afcdf69be858c07",
+    "url": "/credit-card-form/static/media/discover.62706277.png"
+  },
+  {
+    "revision": "87087e5e777b4dc7859e35dcfd8d77aa",
+    "url": "/credit-card-form/static/media/jcb.87087e5e.png"
+  },
+  {
+    "revision": "cdaba15c87239cfe3126dda858c2078f",
+    "url": "/credit-card-form/static/media/mastercard.cdaba15c.png"
+  },
+  {
+    "revision": "40ead9adb1960b406a610a268acf0ebc",
+    "url": "/credit-card-form/static/media/unionpay.40ead9ad.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/credit-card-form/precache-manifest.de0b14d9c8ef3f06f64db193e4e73479.js"
+  "/credit-card-form/precache-manifest.f1693332846cde0b632ae82b2ad7d994.js"
 );
 
 self.addEventListener('message', (event) => {
